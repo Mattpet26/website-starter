0f5cc80f532eb82c80e2335281ad66071dad7fc2
Matthew <Mattpet26@gmail.com>
2020-06-24 20:01:34 -0700

    finally fixxed prompt

--- javascript/adoptnow.js
+++ javascript/adoptnow.js
@@ -1,8 +1,8 @@
 var human = prompt('This is a test, are you a human?');
 var welcome;
-    if (human = "yes") {
+    if (human == "yes") {
         welcome = "Welcome to our site! Please check out our doggos!";
-    } else if (human != null) {
+    } else if (human != "yes") {
         welcome = "We apologize, robots cannot own dogs.";
     }
     document.write(welcome);
